fc3c206387664163a3e1d7ca69c9da48f0347085
Saidov017 <saidov017f@gmail.com>
2020-12-15 15:02:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Complex a=new Complex(45,9);
+        System.out.println(a);
+
+        ComplexAbstractFactory b = new ConcreteFactory();
+        Complex c = b.CreateComplex(564, 453);
+        System.out.println(c);
+
+        ConcreteFactory d = new ConcreteFactory();
+        Complex f = d.CreateComplex(0, 1);
+        System.out.println(f);
+    }
+}
